72df93219ee22fcb65ec09c1e356ef36c4034a6c
Lavich <lobanov.andrey.lavich@gmail.com>
2018-10-25 20:41:28 +0300

    correct connect to bridge

--- package/package.py
+++ package/package.py
@@ -5,6 +5,4 @@ import roslibpy
 
 ros = roslibpy.Ros(host=os.environ.get('ROS_BRIDGE_URI'), port=9090)
 ros.on_ready(lambda: print('Is ROS connected?', ros.is_connected))
-print('start')
 ros.run_forever()
-print('end')
